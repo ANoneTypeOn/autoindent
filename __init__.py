b60db5b9924f8e03e187817de97b52e96bbcf683
Avestor <avestor.git@gmail.com>
2023-01-08 19:11:03 +0000

    Version changed

--- __init__.py
+++ __init__.py
@@ -15,8 +15,8 @@
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-__version__ = "0.1.1"
-__license__ = "GNU General Public License v3.0 (GPL-3.0)"
+__version__ = "0.1.2"
+__license__ = "GNU General Public License v3.0 (GPLv3)"
 __copyright__ = "Copyright (C) 2022-present AvesterHupper <https://github.com/AvesterHupper>"
 
 from .autoindent import Indent
